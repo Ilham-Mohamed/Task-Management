e6fb3eedb82d61afc5271e3984ca9bdf563f79a9
Ilham-Mohamed <ilham1998evr@gmail.com>
2023-03-08 14:58:31 +0530

    navigation page changed

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="antialiased">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
+            <div class="container-fluid ps-5">
                 <a class="navbar-brand" href="{{ route('index') }}">TaskApp</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
